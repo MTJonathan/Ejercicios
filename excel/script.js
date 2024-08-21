568e7e64444d50e86183e99068bbbb14d9b2e45b
MTJonathan <jonathan02330@gmail.com>
2024-08-21 15:12:48 -0500

    Exel

---
+++ excel/script.js
@@ -0,0 +1,36 @@
+const $ = (el) => document.querySelector(el);
+const $$ = (el) => document.querySelectorAll(el);
+
+const ROWS = 10;
+const COLUMNS = 6;
+const FIRST_CHAR_CODE = 65;
+
+const range = (length) => Array.from({ length }, (_, i) => i);
+const getColumn = col => String.fromCharCode(FIRST_CHAR_CODE + col);
+
+const renderSpreadSheet = () => {
+  const $table = $("table");
+  const $head = $("thead");
+  const $body = $("tbody");
+
+  const headerHTML = `<tr>
+        <th></th>
+        ${range(COLUMNS).map(col => `<th>${getColumn(col)}</th>`).join("")}
+    </tr>`;
+
+  $head.innerHTML = headerHTML;
+
+  const bodyHTML = range(ROWS).map(row => {
+    return `<tr>
+        <td>${row + 1}</td>
+        ${range(COLUMNS).map(col => `
+        <td data-x="${col}" data-y="${row}">
+            <span></span>
+            <input type="text" value="" />
+        </td>`).join("")}
+    </tr>`;
+  }).join("");
+  $body.innerHTML = bodyHTML;
+};
+
+renderSpreadSheet();
